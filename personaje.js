a39261e7ec118a40899a12fec3f0debad404c6f5
Elena Megia <elena.megia.canaveras@gmail.com>
2024-04-30 14:47:12 +0200

    limpieza

--- personaje.js
+++ personaje.js
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el ID del personaje de la URL
     const urlParams = new URLSearchParams(window.location.search);
     const personajeId = urlParams.get('id');
-
-    // URL del recurso del personaje
-    const personajeUrl = `https://rickandmortyapi.com/api/character/${personajeId}`;
-
-    // Llamar a la función para mostrar los detalles del personaje
     mostrarDetallesPersonajePorId(personajeId);
 });
 
-// Función para cargar los detalles del personaje por ID
+
 function mostrarDetallesPersonajePorId(id) {
     fetch(`https://rickandmortyapi.com/api/character/${id}`)
         .then(response => response.json())
